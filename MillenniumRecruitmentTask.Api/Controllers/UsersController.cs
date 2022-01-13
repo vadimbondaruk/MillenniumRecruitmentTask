9a7b8b4cb6034da632ea4aa4957874e0a288f3e3
Vadim Bondaruk <v.bondaruk@softteco.com>
2022-01-13 16:08:34 +0300

    Add project files.

---
+++ MillenniumRecruitmentTask.Api/Controllers/UsersController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MillenniumRecruitmentTask.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+
+        private readonly ILogger<UsersController> _logger;
+
+        public UsersController(ILogger<UsersController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
